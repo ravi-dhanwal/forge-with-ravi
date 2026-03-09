a2a4c1d686ef8e6633b3478228deee8a8b48c7d9
Ravi Dhanwal <mail.ravidhanwal@gmail.com>
2026-03-09 05:55:07 +0530

    user reg

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/email', function () {
+    return view('email-notifications.email');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
